c97d1d55842dadb2619655324b9e064c663c7038
kotarmarko <kotarmarko@outlook.com>
2020-04-24 19:33:37 +0200

    Initial commit

---
+++ app/src/main/java/com/markokotar/fakenewsexterminator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.markokotar.fakenewsexterminator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
